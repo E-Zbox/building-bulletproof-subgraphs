37047b23343e974974d6f7ee7d1af40ed0e5c479
E-Zbox <zboxstudios97@gmail.com>
2024-03-11 13:42:03 +0100

    configured handlers to populate entities with required data

--- uniswap-sushi-eth-lp/src/uniswap-v-2-pair.ts
+++ uniswap-sushi-eth-lp/src/uniswap-v-2-pair.ts
@@ -4,99 +4,129 @@ import {
   Mint as MintEvent,
   Swap as SwapEvent,
   Sync as SyncEvent,
-  Transfer as TransferEvent
-} from "../generated/UniswapV2Pair/UniswapV2Pair"
-import { Approval, Burn, Mint, Swap, Sync, Transfer } from "../generated/schema"
+  Transfer as TransferEvent,
+} from "../generated/UniswapV2Pair/UniswapV2Pair";
+import {
+  Approval,
+  Burn,
+  Mint,
+  Swap,
+  Sync,
+  Transfer,
+} from "../generated/schema";
+// utils
+import {
+  createOrLoadApprovalEntity,
+  createOrLoadSwapEntity,
+  createOrLoadTransferEntity,
+  createOrLoadUserEntity,
+} from "./utils";
 
 export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
+  // populating approval entities
+  const approvalEntity = createOrLoadApprovalEntity(event.transaction.hash);
+  approvalEntity.owner = event.params.owner;
+  approvalEntity.spender = event.params.spender;
+  approvalEntity.value = event.params.value;
+  approvalEntity.blockNumber = event.block.number;
+  approvalEntity.blockTimestamp = event.block.timestamp;
+  approvalEntity.transactionHash = event.transaction.hash;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  approvalEntity.save();
 
-  entity.save()
+  // populating user entities
+  const ownerUserEntity = createOrLoadUserEntity(event.params.owner);
+  ownerUserEntity.approvalsAsOwnerTotal.plus(event.params.value);
+
+  ownerUserEntity.save();
+
+  const spenderUserEntity = createOrLoadUserEntity(event.params.spender);
+  spenderUserEntity.approvalsAsSpenderTotal.plus(event.params.value);
+
+  spenderUserEntity.save();
 }
 
 export function handleBurn(event: BurnEvent): void {
-  let entity = new Burn(
+  const burnEntity = new Burn(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-  entity.to = event.params.to
+  );
+  burnEntity.sender = event.params.sender;
+  burnEntity.amount0 = event.params.amount0;
+  burnEntity.amount1 = event.params.amount1;
+  burnEntity.to = event.params.to;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  burnEntity.blockNumber = event.block.number;
+  burnEntity.blockTimestamp = event.block.timestamp;
+  burnEntity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  burnEntity.save();
 }
 
 export function handleMint(event: MintEvent): void {
-  let entity = new Mint(
+  const handleEntity = new Mint(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
+  );
+  handleEntity.sender = event.params.sender;
+  handleEntity.amount0 = event.params.amount0;
+  handleEntity.amount1 = event.params.amount1;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  handleEntity.blockNumber = event.block.number;
+  handleEntity.blockTimestamp = event.block.timestamp;
+  handleEntity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  handleEntity.save();
 }
 
 export function handleSwap(event: SwapEvent): void {
-  let entity = new Swap(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.amount0In = event.params.amount0In
-  entity.amount1In = event.params.amount1In
-  entity.amount0Out = event.params.amount0Out
-  entity.amount1Out = event.params.amount1Out
-  entity.to = event.params.to
+  // populating swap entities
+  const swapEntity = createOrLoadSwapEntity(event.transaction.hash);
+  swapEntity.sender = event.params.sender;
+  swapEntity.amount0In = event.params.amount0In;
+  swapEntity.amount1In = event.params.amount1In;
+  swapEntity.amount0Out = event.params.amount0Out;
+  swapEntity.amount1Out = event.params.amount1Out;
+  swapEntity.to = event.params.to;
+  swapEntity.blockNumber = event.block.number;
+  swapEntity.blockTimestamp = event.block.timestamp;
+  swapEntity.transactionHash = event.transaction.hash;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  swapEntity.save();
 }
 
 export function handleSync(event: SyncEvent): void {
-  let entity = new Sync(
+  const syncEntity = new Sync(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.reserve0 = event.params.reserve0
-  entity.reserve1 = event.params.reserve1
+  );
+  syncEntity.reserve0 = event.params.reserve0;
+  syncEntity.reserve1 = event.params.reserve1;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  syncEntity.blockNumber = event.block.number;
+  syncEntity.blockTimestamp = event.block.timestamp;
+  syncEntity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  syncEntity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
+  // populating transfer entities
+  const transferEntity = createOrLoadTransferEntity(event.transaction.hash);
+  transferEntity.from = event.params.from;
+  transferEntity.to = event.params.to;
+  transferEntity.value = event.params.value;
+  transferEntity.blockNumber = event.block.number;
+  transferEntity.blockTimestamp = event.block.timestamp;
+  transferEntity.transactionHash = event.transaction.hash;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  transferEntity.save();
 
-  entity.save()
+  // populating user entities
+  const senderUserEntity = createOrLoadUserEntity(event.params.from);
+  senderUserEntity.balance.minus(event.params.value);
+
+  senderUserEntity.save();
+
+  const receiverUserEntity = createOrLoadUserEntity(event.params.to);
+  receiverUserEntity.balance.plus(event.params.value);
+
+  receiverUserEntity.save();
 }
