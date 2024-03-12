328edb868325141aa62465edc57d065815cda3f5
E-Zbox <zboxstudios97@gmail.com>
2024-03-12 13:54:31 +0100

    added more tests for user entities

--- uniswap-sushi-eth-lp/tests/uniswap-v-2-pair.test.ts
+++ uniswap-sushi-eth-lp/tests/uniswap-v-2-pair.test.ts
@@ -3,60 +3,136 @@ import {
   describe,
   test,
   clearStore,
-  beforeAll,
-  afterAll
-} from "matchstick-as/assembly/index"
-import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { Approval } from "../generated/schema"
-import { Approval as ApprovalEvent } from "../generated/UniswapV2Pair/UniswapV2Pair"
-import { handleApproval } from "../src/uniswap-v-2-pair"
-import { createApprovalEvent } from "./uniswap-v-2-pair-utils"
+  afterAll,
+  logStore,
+} from "matchstick-as/assembly/index";
+// src
+import {
+  handleApproval,
+  handleSwap,
+  handleTransfer,
+} from "../src/uniswap-v-2-pair";
+// utils
+import {
+  createNewApprovalEvent,
+  createNewSwapEvent,
+  createNewTransferEvent,
+} from "./utils";
 
-// Tests structure (matchstick-as >=0.5.0)
-// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+afterAll(() => {
+  clearStore();
+});
 
-describe("Describe entity assertions", () => {
-  beforeAll(() => {
-    let owner = Address.fromString("0x0000000000000000000000000000000000000001")
-    let spender = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let value = BigInt.fromI32(234)
-    let newApprovalEvent = createApprovalEvent(owner, spender, value)
-    handleApproval(newApprovalEvent)
-  })
+describe("can call mappings using custom events", () => {
+  test("ApprovalEvent", () => {
+    const owner = "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7";
+    const spender = "0xd603fd63255b727b57912de0e251d69367c8eab4";
+    // create mock event
+    const event = createNewApprovalEvent(owner, spender, 345);
 
-  afterAll(() => {
-    clearStore()
-  })
+    // call mapping functions passing the events we just created
+    handleApproval(event);
 
-  // For more test scenarios, see:
-  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+    // let's verify our entity got created correctly
 
-  test("Approval created and stored", () => {
-    assert.entityCount("Approval", 1)
-
-    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    // owner field✅
     assert.fieldEquals(
       "Approval",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      event.transaction.hash.toHexString(),
       "owner",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "Approval",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "spender",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "Approval",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "value",
-      "234"
-    )
+      owner
+    );
 
-    // More assert options:
-    // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
-})
+    // spender field✅
+    assert.fieldEquals(
+      "Approval",
+      event.transaction.hash.toHexString(),
+      "spender",
+      spender
+    );
+
+    // value field✅
+    assert.fieldEquals(
+      "Approval",
+      event.transaction.hash.toHexString(),
+      "value",
+      "345"
+    );
+  });
+
+  test("SwapEvent", () => {
+    const sender = "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7";
+    const to = "0xd603fd63255b727b57912de0e251d69367c8eab4";
+
+    // create mock event
+    const event = createNewSwapEvent(sender, 123, 234, 113, 224, to);
+
+    // call mapping functions passing the events we just created
+    handleSwap(event);
+
+    const id = event.transaction.hash.toHexString();
+
+    // let's verify entity got created correctly
+
+    // sender field✅
+    assert.fieldEquals("Swap", id, "sender", sender);
+
+    // amount0In field✅
+    assert.fieldEquals("Swap", id, "amount0In", "123");
+
+    // amount1In field✅
+    assert.fieldEquals("Swap", id, "amount1In", "234");
+
+    // amount0Out field✅
+    assert.fieldEquals("Swap", id, "amount0Out", "113");
+
+    // amount1Out field✅
+    assert.fieldEquals("Swap", id, "amount1Out", "224");
+
+    // to field✅
+    assert.fieldEquals("Swap", id, "to", to);
+  });
+
+  test("TransferEvent", () => {
+    const from = "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7";
+    const to = "0xd603fd63255b727b57912de0e251d69367c8eab4";
+
+    // create mock event
+    const event = createNewTransferEvent(from, to, 4500);
+
+    // call mapping functions passing the events we just created
+    handleTransfer(event);
+
+    // id corresponds to id used in mapping to create entity
+    const id = event.transaction.hash.toHexString();
+
+    // let's verify entity got created correctly
+
+    // from field✅
+    assert.fieldEquals("Transfer", id, "from", from);
+
+    // to field✅
+    assert.fieldEquals("Transfer", id, "to", to);
+
+    // value field✅
+    assert.fieldEquals("Transfer", id, "value", "4500");
+  });
+});
+
+describe("populated User entity fields", () => {
+  test("TransferEvent updated balance field correctly", () => {
+    const to = "0xd603fd63255b727b57912de0e251d69367c8eab4";
+
+    // logStore();
+
+    assert.fieldEquals("User", to, "balance", "4500");
+  });
+
+  test("TransferEvent updated approvals total", () => {
+    const owner = "0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7";
+    const spender = "0xd603fd63255b727b57912de0e251d69367c8eab4";
+
+    assert.fieldEquals("User", owner, "approvalsAsOwnerTotal", "345");
+    assert.fieldEquals("User", spender, "approvalsAsSpenderTotal", "345");
+  });
+});
